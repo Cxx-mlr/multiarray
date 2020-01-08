4c4a9fca1361ecb48366d7f7e9a71b0c0665961b
Cxx <37257545+Cxx-mlr@users.noreply.github.com>
2020-01-07 19:09:27 -0500

    Update multiarray.hpp

--- include/multiarray.hpp
+++ include/multiarray.hpp
@@ -2,7 +2,6 @@
 #define MULTIARRAY_HPP
 
 #include <iostream>
-#include <array>
 
 #include <type_traits>
 #include <tuple>
@@ -64,46 +63,68 @@ public:
 		return (ns * ... * n);
 	}
 
-	constexpr void fill(const t& value) noexcept {
-		for (std::size_t idx = 0; idx < size(); ++idx) {
-			elems[idx] = value;
-		}
+	constexpr void fill(const t& value) {
+		std::fill_n(zelems, size(), value);
 	}
 
 	constexpr void swap(multiarray& m) noexcept(std::is_nothrow_swappable_v <t>) {
-		elems.swap(m.elems);
+		std::swap(zelems, m.zelems);
 	}
 
 	[[nodiscard]] constexpr auto begin() noexcept {
-		return elems.begin();
+		return std::begin(zelems);
 	}
 
 	[[nodiscard]] constexpr auto end() noexcept {
-		return elems.end();
+		return std::end(zelems);
 	}
 
 	[[nodiscard]] constexpr auto rbegin() noexcept {
-		return elems.rbegin();
+		return std::rbegin(zelems);
 	}
 
 	[[nodiscard]] constexpr auto rend() noexcept {
-		return elems.rend();
+		return std::rend(zelems);
 	}
 
 	[[nodiscard]] constexpr auto cbegin() const noexcept {
-		return elems.cbegin();
+		return std::cbegin(zelems);
 	}
 
 	[[nodiscard]] constexpr auto cend() const noexcept {
-		return elems.cend();
+		return std::cend(zelems);
 	}
 
 	[[nodiscard]] constexpr auto crbegin() const noexcept {
-		return elems.crbegin();
+		return std::crbegin(zelems);
 	}
 
 	[[nodiscard]] constexpr auto crend() const noexcept {
-		return elems.crend();
+		return std::crend(zelems);
+	}
+
+	[[nodiscard]] constexpr t* data() noexcept {
+		return zelems;
+	}
+
+	[[nodiscard]] constexpr const t* data() const noexcept {
+		return zelems;
+	}
+
+	[[nodiscard]] constexpr reference front() noexcept {
+		return zelems[0];
+	}
+
+	[[nodiscard]] constexpr const_reference front() const noexcept {
+		return zelems[0];
+	}
+
+	[[nodiscard]] constexpr reference back() noexcept {
+		return zelems[size() - 1];
+	}
+
+	[[nodiscard]] constexpr const_reference back() const noexcept {
+		return zelems[size() - 1];
 	}
 
 	[[nodiscard]] constexpr reference operator()(std::size_t index, detail::identity <ns>... indexes) noexcept {
@@ -150,24 +171,36 @@ public:
 
 	[[nodiscard]] constexpr const_reference at(std::tuple <std::size_t, detail::identity <ns>...> tupl) const {
 		return std::apply([this](auto... indexes) -> const t& {
-			return *this[to_linear_index(indexes...)];
+			return this->at(indexes...);
 		}, tupl);
 	}
 
 	[[nodiscard]] constexpr reference operator[](std::size_t index) noexcept {
-		return elems[index];
+		return zelems[index];
 	}
 
 	[[nodiscard]] constexpr const_reference operator[](std::size_t index) const noexcept {
-		return elems[index];
+		return zelems[index];
 	}
 
 	[[nodiscard]] constexpr reference min() noexcept {
-		t* min = &elems[0];
+		t* min = &zelems[0];
 
-		for (std::size_t idx = 0; ++idx < size();) {
-			if (elems[idx] < *min) {
-				min = &elems[idx];
+		for (std::size_t idx = 1; idx < size(); ++idx) {
+			if (zelems[idx] < *min) {
+				min = &zelems[idx];
+			}
+		}
+
+		return *min;
+	}
+
+	[[nodiscard]] constexpr const_reference min() const noexcept {
+		const t* min = &zelems[0];
+
+		for (std::size_t idx = 1; idx < size(); ++idx) {
+			if (zelems[idx] < *min) {
+				min = &zelems[idx];
 			}
 		}
 
@@ -175,35 +208,23 @@ public:
 	}
 
 	[[nodiscard]] constexpr reference max() noexcept {
-		t* max = &elems[0];
+		t* max = &zelems[0];
 
-		for (std::size_t idx = 0; ++idx < size();) {
-			if (elems[idx] > *max) {
-				max = &elems[idx];
+		for (std::size_t idx = 1; idx < size(); ++idx) {
+			if (zelems[idx] > * max) {
+				max = &zelems[idx];
 			}
 		}
 
 		return *max;
 	}
 
-	[[nodiscard]] constexpr const_reference min() const noexcept {
-		const t* min = &elems[0];
-
-		for (std::size_t idx = 0; ++idx < size();) {
-			if (elems[idx] < *min) {
-				min = &elems[idx];
-			}
-		}
-
-		return *min;
-	}
-
 	[[nodiscard]] constexpr const_reference max() const noexcept {
-		const t* max = &elems[0];
+		const t* max = &zelems[0];
 
-		for (std::size_t idx = 0; ++idx < size();) {
-			if (elems[idx] > * max) {
-				max = &elems[idx];
+		for (std::size_t idx = 1; idx < size(); ++idx) {
+			if (zelems[idx] > * max) {
+				max = &zelems[idx];
 			}
 		}
 
@@ -211,28 +232,28 @@ public:
 	}
 
 	[[nodiscard]] constexpr t sum() const noexcept {
-		t result = elems[0];
+		t result = zelems[0];
 
-		for (std::size_t idx = 0; ++idx < size();) {
-			result += elems[idx];
+		for (std::size_t idx = 1; idx < size(); ++idx) {
+			result += zelems[idx];
 		}
 	}
 
 #define ARITHMM(OP)\
 	for (std::size_t idx = 0; idx < size(); ++idx) {\
-		elems[idx] OP;\
+		zelems[idx] OP;\
 	}\
 	return *this;
 
 #define ARITHMF(OP)\
 	for (std::size_t idx = 0; idx < m.size(); ++idx) {\
-		m.elems[idx] OP;\
+		m.zelems[idx] OP;\
 	}\
 	return m;
 
 	constexpr multiarray& operator++ () noexcept {
 		for (std::size_t idx = 0; idx < size(); ++idx) {
-			++elems[idx];
+			++zelems[idx];
 		}
 
 		return *this;
@@ -242,7 +263,7 @@ public:
 		multiarray m = *this;
 
 		for (std::size_t idx = 0; idx < size(); ++idx) {
-			elems[idx]++;
+			zelems[idx]++;
 		}
 
 		return m;
@@ -250,7 +271,7 @@ public:
 
 	constexpr multiarray& operator-- () noexcept {
 		for (std::size_t idx = 0; idx < size(); ++idx) {
-			--elems[idx];
+			--zelems[idx];
 		}
 
 		return *this;
@@ -260,7 +281,7 @@ public:
 		multiarray m = *this;
 
 		for (std::size_t idx = 0; idx < size(); ++idx) {
-			elems[idx]--;
+			zelems[idx]--;
 		}
 
 		return m;
@@ -268,7 +289,7 @@ public:
 
 	constexpr multiarray operator+ () noexcept {
 		multiarray m;
-		
+
 		ARITHMF(= +*this[idx]);
 	}
 
@@ -369,40 +390,6 @@ public:
 	constexpr multiarray& operator>>= (const multiarray& m) noexcept {
 		ARITHMM(>>= m[idx]);
 	}
-
-	[[nodiscard]] constexpr static bool index_out_of_bounds(std::size_t index, detail::identity <ns>... indexes) noexcept {
-		return index >= n || ((indexes >= ns) || ...);
-	}
-
-	[[nodiscard]] constexpr static bool index_out_of_bounds(std::tuple <std::size_t, detail::identity <ns>...> tupl) noexcept {
-		return std::apply([](auto... indexes) {
-			return multiarray::index_out_of_bounds(indexes...);
-		}, tupl);
-	}
-
-	[[nodiscard]] constexpr static std::size_t to_linear_index(std::size_t index, detail::identity <ns>... indexes) noexcept {
-		return detail::to_linear_index_impl <n, ns...>(std::make_index_sequence <sizeof...(ns) + 1>(), index, indexes...);
-	}
-
-	[[nodiscard]] constexpr static std::size_t to_linear_index(std::tuple <std::size_t, detail::identity <ns>...> tupl) noexcept {
-		return std::apply([](auto... indexes) {
-			return multiarray::to_linear_index(indexes...);
-		}, tupl);
-	}
-
-	[[nodiscard]] constexpr static auto to_subscript(std::size_t index) noexcept {
-		return detail::to_subscript_impl <n, ns...>(std::make_index_sequence <sizeof...(ns) + 1>(), index);
-	}
-
-	inline friend std::ostream& operator<< (std::ostream& out, const multiarray& m) {
-		for (std::size_t idx = 0; idx < m.size(); ++idx) {
-			std::cout << m.elems[idx] << ' ';
-		}
-
-		return out;
-	}
-
-	std::array <t, (ns * ... * n)> elems;
 
 	[[nodiscard]] constexpr friend auto operator* (cx::multiarray <t, n, ns...> m, const t& value) noexcept {
 		ARITHMF(*= value);
@@ -515,6 +502,40 @@ public:
 	[[nodiscard]] constexpr friend auto operator>> (cx::multiarray <t, n, ns...> m, cx::multiarray <t, n, ns...> mm) noexcept {
 		ARITHMF(>>= mm[idx]);
 	}
+
+	[[nodiscard]] constexpr static bool index_out_of_bounds(std::size_t index, detail::identity <ns>... indexes) noexcept {
+		return index >= n || ((indexes >= ns) || ...);
+	}
+
+	[[nodiscard]] constexpr static bool index_out_of_bounds(std::tuple <std::size_t, detail::identity <ns>...> tupl) noexcept {
+		return std::apply([](auto... indexes) {
+			return multiarray::index_out_of_bounds(indexes...);
+		}, tupl);
+	}
+
+	[[nodiscard]] constexpr static std::size_t to_linear_index(std::size_t index, detail::identity <ns>... indexes) noexcept {
+		return detail::to_linear_index_impl <n, ns...>(std::make_index_sequence <sizeof...(ns) + 1>(), index, indexes...);
+	}
+
+	[[nodiscard]] constexpr static std::size_t to_linear_index(std::tuple <std::size_t, detail::identity <ns>...> tupl) noexcept {
+		return std::apply([](auto... indexes) {
+			return multiarray::to_linear_index(indexes...);
+		}, tupl);
+	}
+
+	[[nodiscard]] constexpr static auto to_subscript(std::size_t index) noexcept {
+		return detail::to_subscript_impl <n, ns...>(std::make_index_sequence <sizeof...(ns) + 1>(), index);
+	}
+
+	inline friend std::ostream& operator<< (std::ostream& out, const multiarray& m) {
+		for (std::size_t idx = 0; idx < m.size(); ++idx) {
+			std::cout << m.zelems[idx] << ' ';
+		}
+
+		return out;
+	}
+
+	t zelems[size()];
 };
 CX_END
 
